794572f955403e8cbe9a752b13a82e9785da0eb9
Praneeth Ganesh <gpraneeth20@gmail.com>
2026-06-01 17:42:05 +0530

    small updates in service

--- repair-service/src/test/java/com/example/repair_service/RepairServiceApplicationTests.java
+++ repair-service/src/test/java/com/example/repair_service/RepairServiceApplicationTests.java
@@ -5,6 +5,9 @@ import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class RepairServiceApplicationTests {
-    @Test
-    void contextLoads() {}
+
+	@Test
+	void contextLoads() {
+	}
+
 }
