54c746f2a6dea7f69c0725d62fb10e7a1579b1d4
Nireeksha575 <nireeksha006@gmail.comgit config --global user.email nireeksha006@gmail.comgit config --global user.email nireeksha006@gmail.com>
2026-06-02 12:43:56 +0530

    used the headers in vendor service to make sure that only vendors access the enpoints

--- device-service/src/main/java/com/example/device_service/Controller/DeviceController.java
+++ device-service/src/main/java/com/example/device_service/Controller/DeviceController.java
@@ -7,14 +7,7 @@ import com.example.device_service.Entity.Device;
 import com.example.device_service.Service.DeviceService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -27,18 +20,17 @@ public class DeviceController {
         this.deviceService = deviceService;
     }
 
-    @PostMapping
-    public ResponseEntity<DeviceDTO> addDevice(@RequestBody DeviceDTO deviceDTO) {
-        Device savedDevice = deviceService.addDevice(deviceDTO);
+    @PostMapping("/{vendorId}")
+    public ResponseEntity<DeviceDTO> addDevice(@RequestBody DeviceDTO deviceDTO,
+                                               @PathVariable long vendorId) {
+        Device savedDevice = deviceService.addDevice(deviceDTO,vendorId);
         return ResponseEntity.status(HttpStatus.CREATED).body(ToDeviceDTO(savedDevice));
-
     }
 
     private DeviceDTO ToDeviceDTO(Device device){
         DeviceDTO deviceDTO=new DeviceDTO();
         deviceDTO.setDeviceName(device.getDeviceName());
         deviceDTO.setDeviceType(device.getDeviceType());
-        deviceDTO.setVendorId(device.getVendorId());
         deviceDTO.setWarrantyExpiry(device.getWarrantyExpiry());
         return deviceDTO;
     }
