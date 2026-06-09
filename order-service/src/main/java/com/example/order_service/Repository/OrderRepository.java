9f2ea52e5983907d30fc8656aa2d7cb2e4d01dbd
Praneeth Ganesh <gpraneeth20@gmail.com>
2026-06-09 18:37:58 +0530

    updated the order-service

--- order-service/src/main/java/com/example/order_service/Repository/OrderRepository.java
+++ order-service/src/main/java/com/example/order_service/Repository/OrderRepository.java
@@ -9,12 +9,7 @@ import org.springframework.data.repository.query.Param;
 import java.util.List;
 
 public interface OrderRepository extends JpaRepository<Orders,Long> {
-    @Query("""
-       SELECT o
-       FROM Orders o
-       WHERE o.vendor_id = :vendorId
-       AND o.status = :status
-       """)
+    @Query("SELECT o FROM Orders o WHERE o.vendorId = :vendorId AND o.status = :status")
     List<Orders> findOrders(
             @Param("vendorId") long vendorId,
             @Param("status") OrderStatus status);
