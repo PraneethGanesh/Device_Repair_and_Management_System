a0d3325674985ce002c9d4619e292805edc40d44
Nireeksha575 <nireeksha006@gmail.comgit config --global user.email nireeksha006@gmail.comgit config --global user.email nireeksha006@gmail.com>
2026-06-11 21:05:30 +0530

    fixed a bug

--- notification-service/src/main/java/com/example/notification_service/Service/NotificationService.java
+++ notification-service/src/main/java/com/example/notification_service/Service/NotificationService.java
@@ -9,6 +9,7 @@ import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.mail.MailException;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestClient;
 
@@ -28,7 +29,7 @@ public class NotificationService {
         this.mailSender = mailSender;
     }
 
-
+    @Async
     public void sendBulkEmail(List<String> toEmails, String subject, String body) {
         try {
             SimpleMailMessage message = new SimpleMailMessage();
@@ -45,7 +46,7 @@ public class NotificationService {
             LOGGER.error("❌ Failed to send email: {}", e.getMessage());
         }
     }
-
+    @Async
     public void sendSingleEmail(String toEmail, String subject, String body) {
         try {
             SimpleMailMessage message = new SimpleMailMessage();
