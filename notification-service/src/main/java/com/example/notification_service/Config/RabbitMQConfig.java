4b0b676027f7b35a0b7c9df8ce030c61709441bc
Praneeth Ganesh <gpraneeth20@gmail.com>
2026-06-17 08:54:48 +0530

    new update

--- notification-service/src/main/java/com/example/notification_service/Config/RabbitMQConfig.java
+++ notification-service/src/main/java/com/example/notification_service/Config/RabbitMQConfig.java
@@ -1,4 +1,4 @@
-package com.example.notification_service.Config;
+package com.example.notification_service.config;
 
 import org.springframework.amqp.core.*;
 import org.springframework.amqp.rabbit.connection.ConnectionFactory;
