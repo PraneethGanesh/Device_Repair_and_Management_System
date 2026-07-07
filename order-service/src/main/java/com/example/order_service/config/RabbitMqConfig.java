6f6370c0eae9b9bbdf5c0885724388951297eea9
Praneeth Ganesh <gpraneeth20@gmail.com>
2026-07-07 10:54:06 +0530

    new updates make to the pom.xml because it simplifies dependency management

--- order-service/src/main/java/com/example/order_service/config/RabbitMqConfig.java
+++ order-service/src/main/java/com/example/order_service/config/RabbitMqConfig.java
@@ -3,7 +3,7 @@ package com.example.order_service.config;
 import org.springframework.amqp.core.TopicExchange;
 import org.springframework.amqp.rabbit.connection.ConnectionFactory;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
-import org.springframework.amqp.support.converter.JacksonJsonMessageConverter;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
 import org.springframework.amqp.support.converter.MessageConverter;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
@@ -30,7 +30,7 @@ public class RabbitMqConfig {
 
     @Bean
     public MessageConverter getMessageConverter(){
-        return new JacksonJsonMessageConverter();
+        return new Jackson2JsonMessageConverter();
     }
 
 
